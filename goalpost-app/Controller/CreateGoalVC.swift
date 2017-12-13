a1c18ad924d53b6cf456927e5501ad5380466b12
Brian  Crowley <briancrowley@Brians-MacBook-Pro.local>
2017-12-13 11:05:01 +0000

    UIView and UIButton extensions

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -13,15 +13,28 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var shortTermButton: UIButton!
     @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton : UIButton!
+    
+    var goalType : GoalType = .shortTerm
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
+    
     @IBAction func shortTermButtonWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     @IBAction func longTermButtonWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
     }
+    
     @IBAction func nextButtonWasPressed(_ sender: Any) {
     }
     @IBAction func backButtonWasPressed(_ sender: Any) {
---
+++ goalpost-app/Extensions/UIButtonExt.swift
@@ -0,0 +1,21 @@
+//
+//  UIButtonExt.swift
+//  goalpost-app
+//
+//  Created by Brian  Crowley on 13/12/2017.
+//  Copyright © 2017 Brian Crowley. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setSelectedColor () {
+        backgroundColor = #colorLiteral(red: 0.3760718405, green: 0.794663012, blue: 0.3411160707, alpha: 1)
+    }
+    
+    func setDeselectedColor() {
+        backgroundColor = #colorLiteral(red: 0.3760718405, green: 0.794663012, blue: 0.3411160707, alpha: 0.5)
+    }
+
+}
